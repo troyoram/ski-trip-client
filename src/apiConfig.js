9d533fa1f7d27faf3a1d9e96b9c461650b4b247a
troyoram <oram0720@gmail.com>
2018-12-01 20:59:48 -0500

    Update production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://ski-trip-server.herokuapp.com/',
+  production: 'https://ski-trip-server.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
